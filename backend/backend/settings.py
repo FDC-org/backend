2b02c3c7a8eb9e8251c1cc7231a1fa1bf8a72d62
dharmatejan-dev <admin@dharmatejan.in>
2025-04-13 21:42:34 +0530

    created app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)*^2#d7dy9w51q_f7=#s45a$x_#&sk0%9k%xj7!^%^n()g+l=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://backend-9py7.onrender.com']
+ALLOWED_HOSTS = ['backend-9py7.onrender.com','127.0.0.1']
 
 
 # Application definition
