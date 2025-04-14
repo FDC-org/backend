8184d0bb17c7a8bccd77395327c0323120f0db0a
dharmatejan-dev <admin@dharmatejan.in>
2025-04-14 22:55:37 +0530

    created app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'FDC',
+        'USER': 'fdc_admin',
+        'PASSWORD': 'AVNS_QwwafU3ydiususIFM24',
+        'HOST': 'mysql-17e4e0d2-fdc-db.k.aivencloud.com',   # Or your DB host
+        'PORT': '18353',        # Default MySQL port
     }
 }
 
