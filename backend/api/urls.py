c80a43a207fb102c40864b68322c3748f9fe8f5d
dharmatejan-dev <admin@dharmatejan.in>
2025-05-07 23:46:24 +0530

    track

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import views
 from .views import user
 from .views import basic_api_views
-from .views import inscan,outscan
+from .views import inscan, outscan
 
 urlpatterns = [
     # user api
@@ -17,7 +17,8 @@ urlpatterns = [
     path('getmanifestnumber/', basic_api_views.GetManifestNumber.as_view()),
     path('vehicledetails/', basic_api_views.VehicleDetails.as_view()),
     path('gethublist/', basic_api_views.GetHubList.as_view()),
-
+    path('verify_token/', basic_api_views.VerifyToken.as_view()),
+    path('track/<slug:awbno>', basic_api_views.Track.as_view()),
 
     # inscan
     path('inscan/', inscan.Inscan.as_view()),
@@ -26,7 +27,7 @@ urlpatterns = [
     # outscan
     path('outscan/<slug:date>', outscan.OutScan.as_view()),
     path('outscan/', outscan.OutScan.as_view()),
-    path('manifestdata/<slug:manifest_number>',outscan.ManifestData.as_view()),
+    path('manifestdata/<slug:manifest_number>', outscan.ManifestData.as_view()),
 
     path('test/', user.test)
 ]
