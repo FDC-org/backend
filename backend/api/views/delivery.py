df8cab1a2872d4f5177bac2ad017461aa20d7d9e
dharmatejan <admin@dharmatejan.in>
2025-12-29 17:46:49 +0530

    drs and delivery

--- backend/api/views/delivery.py
+++ backend/api/views/delivery.py
@@ -89,12 +89,18 @@ class Delivered(APIView):
                 date = r.data['date']
                 for awb in awbno:
                     DeliveryDetails.objects.create(awbno = awb,status = 'delivered',recievername = receivername,image = image,recievernumber=receivernumber,date= date)
+                dd = DrsDetails.objects.get(awbno=awbno)
+                dd.status = True
+                dd.save()
             elif awbstatus == 'undelivered' or  awbstatus == 'rto':
                 reason = r.data['reason']
                 statusre = "undelivered" if awbstatus == 'undelivered' else "rto"
                 for awb in awbno:
                     deliverdordrs.objects.filter(awbno=awb).delete()
                     DeliveryDetails.objects.create(awbno=awb, status=statusre, reason=reason)
+                dd = DrsDetails.objects.get(awbno=awbno)
+                dd.status = True
+                dd.save()
             else:
                 return Response({"status":"invalid status"}, status=awbstatus.HTTP_400_BAD_REQUEST)
             return Response({"status":"success",}, status=status.HTTP_201_CREATED)
