5688277be6d3a84034389fe4df87c4aa9161863f
dharmatejan <admin@dharmatejan.in>
2026-01-11 01:49:16 +0530

    drs and delivery

--- backend/api/views/inscan.py
+++ backend/api/views/inscan.py
@@ -3,14 +3,20 @@ import datetime
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from ..models import UserDetails, InscanModel, BookingDetails
+
 
 class Inscan(APIView):
     def get(self, r, date):
         if not date:
-            return Response({'error': 'not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        inscandata = InscanModel.objects.filter(inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
-                                                date__date=date)
+            return Response(
+                {"error": "not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
+        inscandata = InscanModel.objects.filter(
+            inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
+            date__date=date,
+        )
         data = []
         for i in inscandata:
             try:
@@ -22,27 +28,42 @@ class Inscan(APIView):
                 pcs = ""
                 wt = ""
                 doc_type = ""
-            data.append({"awbno": i.awbno, "date": i.date, "type": doc_type, "pcs": pcs, "wt": wt})
+            data.append(
+                {
+                    "awbno": i.awbno,
+                    "date": i.date,
+                    "type": doc_type,
+                    "pcs": pcs,
+                    "wt": wt,
+                }
+            )
         return Response({"status": "success", "data": data})
 
     def post(self, r):
-        awb_no = r.data['awbno']
+        awb_no = r.data["awbno"]
         branch_code = UserDetails.objects.get(user=r.user)
         try:
             for i in awb_no:
                 dt_naive = datetime.datetime.strptime(i[0], "%d-%m-%Y, %H:%M:%S")
-                InscanModel.objects.create(awbno=i[1], inscaned_branch_code=branch_code.code,date=dt_naive)
+                InscanModel.objects.create(
+                    awbno=i[1], inscaned_branch_code=branch_code.code, date=dt_naive
+                )
             return Response({"status": "success"}, status=status.HTTP_201_CREATED)
         except Exception as e:
             print(e)
             return Response({"status": "error"}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
+
 class InscanMobile(APIView):
     def get(self, r, date):
         if not date:
-            return Response({'error': 'not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        inscandata = InscanModel.objects.filter(inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
-                                                date__date=date)
+            return Response(
+                {"error": "not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
+        inscandata = InscanModel.objects.filter(
+            inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
+            date__date=date,
+        )
         data = []
         for i in inscandata:
             try:
@@ -54,17 +75,27 @@ class InscanMobile(APIView):
                 pcs = ""
                 wt = ""
                 doc_type = ""
-            data.append({"awbno": i.awbno, "date": i.date, "type": doc_type, "pcs": pcs, "wt": wt})
+            data.append(
+                {
+                    "awbno": i.awbno,
+                    "date": i.date,
+                    "type": doc_type,
+                    "pcs": pcs,
+                    "wt": wt,
+                }
+            )
         return Response({"status": "success", "data": data})
 
     def post(self, r):
-        awb_no = r.data['awbno']
+        awb_no = r.data["awbno"]
         branch_code = UserDetails.objects.get(user=r.user)
-        date = r.data['date']
+        date = r.data["date"]
         dt_naive = datetime.datetime.strptime(date, "%d-%m-%Y, %H:%M:%S")
         try:
             for i in awb_no:
-                InscanModel.objects.create(awbno=i, inscaned_branch_code=branch_code.code,date=dt_naive)
+                InscanModel.objects.create(
+                    awbno=i, inscaned_branch_code=branch_code.code, date=dt_naive
+                )
             return Response({"status": "success"}, status=status.HTTP_201_CREATED)
         except Exception as e:
             print(e)
--- backend/api/views/outscan.py
+++ backend/api/views/outscan.py
@@ -1,49 +1,71 @@
 import datetime
 
-from django.utils.timezone import make_aware
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..models import UserDetails, OutscanModel, ManifestDetails, Vehicle_Details, BookingDetails_temp, BookingDetails
+from ..models import (
+    UserDetails,
+    OutscanModel,
+    ManifestDetails,
+    Vehicle_Details,
+    BookingDetails,
+)
 
 
 class OutScan(APIView):
     def get(self, r, date):
         if not date:
-            return Response({'error': 'not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        manifesstdata = ManifestDetails.objects.filter(inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
-                                                       date__date=date)
+            return Response(
+                {"error": "not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
+        manifesstdata = ManifestDetails.objects.filter(
+            inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
+            date__date=date,
+        )
         data = []
         for i in manifesstdata:
             vehicle_num = ""
             if i.vehicle_number:
                 vehicle_num = i.vehicle_number.vehiclenumber
-            data.append({"date": i.date, "manifestnumber": i.manifestnumber,
-                         "tohub": UserDetails.objects.get(code=i.tohub_branch_code).code_name,
-                         "vehicle_number": vehicle_num})
+            data.append(
+                {
+                    "date": i.date,
+                    "manifestnumber": i.manifestnumber,
+                    "tohub": UserDetails.objects.get(
+                        code=i.tohub_branch_code
+                    ).code_name,
+                    "vehicle_number": vehicle_num,
+                }
+            )
         return Response({"status": "success", "data": data})
 
     def post(self, r):
-        awb_no = r.data['awbno']
-        manifest_number = r.data['manifest_number']
-        vehicle_number = r.data['vehicle_number']
+        awb_no = r.data["awbno"]
+        manifest_number = r.data["manifest_number"]
+        vehicle_number = r.data["vehicle_number"]
         tohub = r.data["tohub"]
         branch_code = UserDetails.objects.get(user=r.user)
-        date = r.data['date']
+        date = r.data["date"]
         try:
             dt_naive = datetime.datetime.strptime(date, "%d-%m-%Y, %H:%M:%S")
-            manifest = ManifestDetails.objects.create(date=dt_naive, inscaned_branch_code=branch_code.code,
-                                                      tohub_branch_code=UserDetails.objects.get(code_name=tohub).code,
-                                                      manifestnumber=manifest_number,
-                                                      vehicle_number=Vehicle_Details.objects.get(
-                                                          vehiclenumber=vehicle_number))
+            manifest = ManifestDetails.objects.create(
+                date=dt_naive,
+                inscaned_branch_code=branch_code.code,
+                tohub_branch_code=UserDetails.objects.get(code_name=tohub).code,
+                manifestnumber=manifest_number,
+                vehicle_number=Vehicle_Details.objects.get(
+                    vehiclenumber=vehicle_number
+                ),
+            )
             for i in awb_no:
                 OutscanModel.objects.create(awbno=i[2], manifestnumber=manifest)
             branch_code.manifestnumber = str(int(branch_code.manifestnumber) + 1)
             branch_code.save()
-            return Response({"status": "success", "manifest_number": branch_code.manifestnumber},
-                            status=status.HTTP_201_CREATED)
+            return Response(
+                {"status": "success", "manifest_number": branch_code.manifestnumber},
+                status=status.HTTP_201_CREATED,
+            )
         except Exception as e:
             print(e)
             ma = ManifestDetails.objects.filter(manifestnumber=manifest_number)
@@ -51,40 +73,58 @@ class OutScan(APIView):
                 ma[0].delete()
             return Response({"status": "error"}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
+
 class OutScanMobile(APIView):
     def get(self, r, date):
         if not date:
-            return Response({'error': 'not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        manifesstdata = ManifestDetails.objects.filter(inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
-                                                       date__date=date)
+            return Response(
+                {"error": "not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
+        manifesstdata = ManifestDetails.objects.filter(
+            inscaned_branch_code=UserDetails.objects.get(user=r.user).code,
+            date__date=date,
+        )
         data = []
 
         for i in manifesstdata:
             vehicle_num = ""
             if i.vehicle_number:
                 vehicle_num = i.vehicle_number.vehiclenumber
-            data.append({"date": i.date, "manifestnumber": i.manifestnumber,
-                         "tohub": UserDetails.objects.get(code=i.tohub_branch_code).code_name,"vehicle_number":vehicle_num})
+            data.append(
+                {
+                    "date": i.date,
+                    "manifestnumber": i.manifestnumber,
+                    "tohub": UserDetails.objects.get(
+                        code=i.tohub_branch_code
+                    ).code_name,
+                    "vehicle_number": vehicle_num,
+                }
+            )
         return Response({"status": "success", "data": data})
 
     def post(self, r):
-        awb_no = r.data['awbno']
-        manifest_number = r.data['manifest_number']
+        awb_no = r.data["awbno"]
+        manifest_number = r.data["manifest_number"]
         # vehicle_number = r.data['vehicle_number']
         tohub = r.data["tohub"]
         branch_code = UserDetails.objects.get(user=r.user)
-        date = r.data['date']
+        date = r.data["date"]
         try:
             dt_naive = datetime.datetime.strptime(date, "%d-%m-%Y, %H:%M:%S")
-            manifest = ManifestDetails.objects.create(date=dt_naive, inscaned_branch_code=branch_code.code,
-                                                      tohub_branch_code=UserDetails.objects.get(code_name=tohub).code,
-                                                      manifestnumber=manifest_number)
+            manifest = ManifestDetails.objects.create(
+                date=dt_naive,
+                inscaned_branch_code=branch_code.code,
+                tohub_branch_code=UserDetails.objects.get(code_name=tohub).code,
+                manifestnumber=manifest_number,
+            )
             for i in awb_no:
                 OutscanModel.objects.create(awbno=i, manifestnumber=manifest)
             branch_code.manifestnumber = str(int(branch_code.manifestnumber) + 1)
             branch_code.save()
-            return Response({"status": "success", "manifest_number": branch_code.manifestnumber},
-                            status=status.HTTP_201_CREATED)
+            return Response(
+                {"status": "success", "manifest_number": branch_code.manifestnumber},
+                status=status.HTTP_201_CREATED,
+            )
         except Exception as e:
             print(e)
             ma = ManifestDetails.objects.filter(manifestnumber=manifest_number)
@@ -100,13 +140,20 @@ class OutScanMobile(APIView):
 class ManifestData(APIView):
     def get(self, r, manifest_number):
         if not manifest_number:
-            return Response({'error': 'not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response(
+                {"error": "not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
 
         data = []
         manifest_details = ManifestDetails.objects.get(manifestnumber=manifest_number)
-        vehicle_num = manifest_details.vehicle_number.vehiclenumber if manifest_details.vehicle_number else ""
+        vehicle_num = (
+            manifest_details.vehicle_number.vehiclenumber
+            if manifest_details.vehicle_number
+            else ""
+        )
         for i in OutscanModel.objects.filter(
-                manifestnumber=ManifestDetails.objects.get(manifestnumber=manifest_number)):
+            manifestnumber=ManifestDetails.objects.get(manifestnumber=manifest_number)
+        ):
             awbdetails = BookingDetails.objects.filter(awbno=i.awbno)
             pcs = ""
             wt = ""
@@ -114,6 +161,14 @@ class ManifestData(APIView):
                 pcs = awbdetails[0].pcs
                 wt = awbdetails[0].wt
             data.append({"awbno": i.awbno, "pcs": pcs, "wt": wt})
-        return Response({"status": "success", "date": manifest_details.date,
-                         "tohub": UserDetails.objects.get(code=manifest_details.tohub_branch_code).code_name,
-                         "vehicle_number":vehicle_num, "awbno": data})
+        return Response(
+            {
+                "status": "success",
+                "date": manifest_details.date,
+                "tohub": UserDetails.objects.get(
+                    code=manifest_details.tohub_branch_code
+                ).code_name,
+                "vehicle_number": vehicle_num,
+                "awbno": data,
+            }
+        )
